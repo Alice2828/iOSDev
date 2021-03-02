b7bd7039f1fe3a996d1846101496c311c389484a
curer13 <era.owl@gmail.com>
2021-03-02 12:44:02 +0600

    initial

---
+++ KBTUApp/KBTUApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KBTUApp
+//
+//  Created by User on 02.03.2021.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
